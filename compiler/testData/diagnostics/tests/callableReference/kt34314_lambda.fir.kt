8a4bdb6a3a5bb1f48bb1749cbf1d1f98053f5223
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2020-01-09 17:12:45 +0300

    [NI] Update test data to match FIR, add test

--- compiler/testData/diagnostics/tests/callableReference/kt34314_lambda.fir.kt
+++ compiler/testData/diagnostics/tests/callableReference/kt34314_lambda.fir.kt
@@ -1,6 +1,8 @@
 // !WITH_NEW_INFERENCE
 // !DIAGNOSTICS: -UNUSED_VARIABLE
 
+fun <T> materialize(): T = TODO()
+
 fun main() {
-    val x = run { ::run } // no error
+    val x = run { materialize() }
 }
